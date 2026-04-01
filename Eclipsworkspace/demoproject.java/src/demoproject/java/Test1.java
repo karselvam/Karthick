f70bb276669931fd9a95b673c08882bdd108dc00
Karthick Selvam <kselvam_ext@Royalenfield.com>
2026-04-01 08:53:41 +0530

    Eclips testing Java project

---
+++ Eclipsworkspace/demoproject.java/src/demoproject/java/Test1.java
@@ -0,0 +1,28 @@
+package demoproject.java;
+
+public class Test1 {
+	int marks;
+	String name;
+	
+	test(int a, String b){
+		
+		System.out.println();
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	Test1 obj= new Test1(22, "kar");
+	
+
+	
+	
+	
+	
+	
+	
+
+	}
+
+}
